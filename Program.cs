8036b59573b81a7f3d847ae3a5170728bcd0404c
pranalikante123 <pranalikante21@gmail.com>
2022-09-22 16:44:08 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace myproject
+{
+    class Program
+    {
+        class Mascot
+        {
+            public string? name {get; set;}
+            public string? team {get; set;}
+            public string? phrase {get; set;}
+        }
+        static void Main(string[] args)
+        {
+            Mascot nugetmascot =new Mascot()
+            {
+                name = "Nuget Warrior",
+                team = "Nuget",
+                phrase = "where packages come to life",
+            };
+            string json = JsonConvert.SerializeObject(nugetmascot, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
